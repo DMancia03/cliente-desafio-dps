5804e0da407d692e3d45a14353cf2253cc0a79c9
DMancia03 <diego.mancia.03@gmail.com>
2024-10-17 22:16:50 -0600

    Cambiando nombre de aplicación

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import VerSolicitudes from './src/screens/VerSolicitudes';
+import VerSolicitud from './src/screens/VerSolicitud';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='VerSolicitudes'
+          component={VerSolicitudes}
+          options={{
+            title: 'Ver solicitudes'
+          }}
+        />
+        <Stack.Screen
+          name='VerSolicitud'
+          component={VerSolicitud}
+          initialParams={{ id: 0 }}
+          options={{
+            title: 'Ver solicitud'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/VerSolicitudes.js
+++ src/screens/VerSolicitudes.js
@@ -1,1 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Button } from 'react-native';
+
+const VerSolicitudes = ({ navigation, route }) => {
+    return (
+        <View>
+            <Text>Ver Solicitudes</Text>
+        </View>
+    )
+}
+
+export default VerSolicitudes;
