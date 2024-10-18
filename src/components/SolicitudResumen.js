ad386f8b8748eecd2b881bed824e5472b6332469
DMancia03 <diego.mancia.03@gmail.com>
2024-10-17 22:53:47 -0600

    Agregando diseño a solicitud resumen

--- src/components/SolicitudResumen.js
+++ src/components/SolicitudResumen.js
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import colors from "../styles/colors";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const SolicitudResumen = ({ solicitud, verSolicitud }) => {
     return (
-        <View>
-            <Text>{solicitud.nombre}</Text>
-            <Text>{solicitud.telefono}</Text>
-            <Text>{solicitud.estado}</Text>
-            <TouchableOpacity onPress={() => {verSolicitud(solicitud)}}>
-                <Text>Ver detalle</Text>
+        <View style={styles.container}>
+            <Text><Text style={styles.bolder}>Usuario:</Text> {solicitud.nombre}</Text>
+            <Text><Text style={styles.bolder}>Teléfono:</Text> {solicitud.telefono}</Text>
+            <Text><Text style={styles.bolder}>Estado de solicitud:</Text> {solicitud.estado}</Text>
+            <TouchableOpacity style={styles.buttonDetails} onPress={() => {verSolicitud(solicitud)}}>
+                <Text style={styles.buttonDetailsText}><Icon name="eye" size={20} /> Ver detalle</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
 export default SolicitudResumen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.WHITE,
+        borderRadius: 10,
+        padding: 20,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10
+    },
+    buttonDetails: {
+        backgroundColor: colors.PRYMARY_COLOR,
+        padding: 10,
+        borderRadius: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonDetailsText: {
+        color: colors.WHITE,
+    },
+    bolder: {
+        fontWeight: 'bold'
+    }
+})
