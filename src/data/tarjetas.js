3510e6a75f94d877faa9a7ff1b81485d3db37074
DMancia03 <diego.mancia.03@gmail.com>
2024-10-18 21:18:51 -0600

    Agregando data de productos y tarjetas

--- src/data/tarjetas.js
+++ src/data/tarjetas.js
@@ -1,89 +1,28 @@
 export default {
-    riesgo_test: [{
-        id: 'C001',
-        title: 'Clásica',
-        description: 'Tarjeta de crédito clásica',
-        image: require('../../assets/img-cards/clasica.png'),
-    },
-    {
-        id: 'C002',
-        title: 'Oro',
-        description: 'Tarjeta de crédito oro',
-        image: require('../../assets/img-cards/oro.png'),
-    },
-    {
-        id: 'C003',
-        title: 'Platinum',
-        description: 'Tarjeta de crédito platinum',
-        image: require('../../assets/img-cards/platinum.png'),
-    },
-    {
-        id: 'C004',
-        title: 'Black',
-        description: 'Tarjeta de crédito black',
-        image: require('../../assets/img-cards/black.png'),
-    }],
-    riesgo_default: [],
-    riesgo_extremo: [],
-    riesgo_alto: [],
-    riesgo_suficiente: [{
-        id: 'C001',
-        title: 'Clásica',
-        description: 'Tarjeta de crédito clásica',
-        image: require('../../assets/img-cards/clasica.png'),
-    },],
-    riesgo_bueno: [{
-        id: 'C001',
-        title: 'Clásica',
-        description: 'Tarjeta de crédito clásica',
-        image: require('../../assets/img-cards/clasica.png'),
-    },
-    {
-        id: 'C002',
-        title: 'Oro',
-        description: 'Tarjeta de crédito oro',
-        image: require('../../assets/img-cards/oro.png'),
-    },],
-    riesgo_muy_bueno: [{
-        id: 'C001',
-        title: 'Clásica',
-        description: 'Tarjeta de crédito clásica',
-        image: require('../../assets/img-cards/clasica.png'),
-    },
-    {
-        id: 'C002',
-        title: 'Oro',
-        description: 'Tarjeta de crédito oro',
-        image: require('../../assets/img-cards/oro.png'),
-    },
-    {
-        id: 'C003',
-        title: 'Platinum',
-        description: 'Tarjeta de crédito platinum',
-        image: require('../../assets/img-cards/platinum.png'),
-    },],
-    riesgo_excelente: [{
-        id: 'C001',
-        title: 'Clásica',
-        description: 'Tarjeta de crédito clásica',
-        image: require('../../assets/img-cards/clasica.png'),
-    },
-    {
-        id: 'C002',
-        title: 'Oro',
-        description: 'Tarjeta de crédito oro',
-        image: require('../../assets/img-cards/oro.png'),
-    },
-    {
-        id: 'C003',
-        title: 'Platinum',
-        description: 'Tarjeta de crédito platinum',
-        image: require('../../assets/img-cards/platinum.png'),
-    },
-    {
-        id: 'C004',
-        title: 'Black',
-        description: 'Tarjeta de crédito black',
-        image: require('../../assets/img-cards/black.png'),
-    }],
+    riesgo_global: [
+        {
+            id: 'C001',
+            title: 'Clásica',
+            description: 'Tarjeta de crédito clásica',
+            image: require('../../assets/img-cards/clasica.png'),
+        },
+        {
+            id: 'C002',
+            title: 'Oro',
+            description: 'Tarjeta de crédito oro',
+            image: require('../../assets/img-cards/oro.png'),
+        },
+        {
+            id: 'C003',
+            title: 'Platinum',
+            description: 'Tarjeta de crédito platinum',
+            image: require('../../assets/img-cards/platinum.png'),
+        },
+        {
+            id: 'C004',
+            title: 'Black',
+            description: 'Tarjeta de crédito black',
+            image: require('../../assets/img-cards/black.png'),
+        }
+    ],
 }
